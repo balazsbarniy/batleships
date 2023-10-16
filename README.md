c9c2fb5b17d9c5a1245b77a370361f817de72230
Barna <128509430+balazsbarniy@users.noreply.github.com>
2023-10-16 10:50:35 +0100

    add table, and creat function for the user

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# batleships
-battleship game for pp3  project. 
+# Batleships game
 
-## Start making my very first pyton project. simple battleship game.
+Battleship game for pp3  project. 
+Start making my very first pyton project. simple battleship game.
 
-## for first step i creat the table , wich is a  10x10 grid.
+1. Table
+for first step i creat the table, wich is a 10x10 grid, and add the function to write the hit,miss,and comp on the table.
+
+
+2. Creat a function for the user
+ wich is an intriger because we need a nr not a letter/negativ etc. nr have  to be between 0 and 99  
+ if the user enter incorrect fomrat: error message  show up " Incorrect!! please try again". 
+ also have to check if the user enter was used before.
--- main.py
+++ main.py
@@ -1,5 +1,69 @@
-print("battleships")
-print(" 1 2 3 4 5 6 7 8 9")
 
-for x in range(10):
-    print(x," "," _ "*10)
+def get_shot(guesses):
+
+    ok = "n"
+    while ok == "n":
+        try:
+            shot = input("Please enter a nr :" )
+            shot = int(shot)
+            if shot < 0 or shot > 99:
+                print("please type a nr between 0/99")
+            elif shot in guesses :
+                print("incorrect !!  please try again ! ")
+            else:
+                ok = "y"
+                break
+        except:
+            print("incorrect !!  please try again ! ")
+    return shot
+
+def show_board(hit,miss,comp):
+    print("            battleships    ")
+    print()
+    print("     0  1  2  3  4  5  6  7  8  9")
+
+
+    place = 0
+    for x in range(10):
+        row = ""
+        for y in range(10):
+            ch = " _ "
+            if place in miss:
+                ch = " x "
+            elif place  in hit:
+                ch = " o "
+            elif place  in comp:
+                ch = " 0 "
+
+            row = row + ch
+            place = place + 1
+        print(x," ",row)
+
+
+
+def check_shot(shot,boat1,hit,miss,comp):
+
+    if shot in boat1:
+        boat1.remove(shot)
+        if len(boat1) > 0:
+            hit.append(shot)
+        else:
+            comp.append(shot)
+    else:
+        miss.append(shot)
+
+    return boat1,hit,miss,comp
+
+
+
+
+boat1 = [45,46,47]
+hit = []
+miss = []
+comp = []
+
+for i in range(10):
+    guesses = hit + miss + comp
+    shot = get_shot(guesses)
+    boat1,hit,miss,comp = check_shot(shot,boat1,hit,miss,comp)
+    show_board(hit,miss,comp)
